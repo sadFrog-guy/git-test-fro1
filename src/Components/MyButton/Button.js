1ea4ed6bbfad51eeb36282e16b7ec10e5ae0b965
sadFrog-guy <adikokey@mail.ru>
2024-03-23 02:18:54 +0600

    *

--- src/Components/MyButton/Button.js
+++ src/Components/MyButton/Button.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import style from "./Button.module.css" 
 
-export default function ({children, disabled=false}) {
+export default function Button({children, disabled=false}) {
   return (
     <button className={disabled ? [style.button, style.disabled].join(' ') : style.button}>{children}</button>
   )
--- src/transitions.js
+++ src/transitions.js
@@ -11,7 +11,7 @@ const transition = (OgComponent) => {
                 initial={{scaleY: 0}}
                 animate={{scaleY: 0}}
                 exit={{scaleY: 1}}
-                transition={{duration: 0.5, ease: [0.22, 1, 0.36, 1]}}
+                transition={{duration: 0.6, ease: [0.22, 1, 0.36, 1]}}
             />
 
             <motion.div
@@ -19,7 +19,7 @@ const transition = (OgComponent) => {
                 initial={{scaleY: 1}}
                 animate={{scaleY: 0}}
                 exit={{scaleY: 0}}
-                transition={{duration: 0.5, ease: [0.22, 1, 0.36, 1]}}
+                transition={{duration: 0.6, ease: [0.22, 1, 0.36, 1]}}
             />
         </>
     )
